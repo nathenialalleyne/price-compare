098e7787b26df76bd16b2fc4c7ca049e750cbeb6
nateachino <natealleyne88@gmail.com>
2023-02-01 01:12:12 -0500

    Fixed dockerfile changed scrapper to find a tags

--- server/server.mjs
+++ server/server.mjs
@@ -4,19 +4,36 @@ import puppeteer from "puppeteer";
 const app = express();
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/chromium-browser",
+    headless: true,
+    executablePath: "/usr/bin/google-chrome",
+    args: ["--no-sandbox", "--disable-gpu"],
   });
   const page = await browser.newPage();
-  await page.goto("https://www.bestbuy.com/");
-  page.screenshot({ path: "screenshot.jpg" });
+  await page.goto("https://www.bestbuy.com/", {
+    waitUntil: "networkidle2",
+    timeout: 0,
+  });
+
+  const html = await page.evaluate(() => {
+    Array.from(document.body.querySelectorAll("a"), (e) => {
+      e.href;
+    });
+  });
+
+  console.log(html);
+
   await browser.close();
 })();
+
 app.get("/test", (req, res) => {
   res.json({
     test: "test",
     test2: "testsets",
   });
 });
-app.listen(5000, () => {
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   console.log("running on port 5000");
 });
